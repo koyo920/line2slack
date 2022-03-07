5048f090e7d7a10ea8e706a7dc9f37e0a64b503c
koyowakebe <oeigivin@gmail.com>
2022-03-07 23:55:06 +0900

    fix

--- main.py
+++ main.py
@@ -1,98 +1,66 @@
 import os
 
-import requests
+import requests, json
 import slackweb
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage
+from linebot.models import MessageEvent, TextMessage, ImageMessage
 
 app = Flask(__name__)
 
-# heroku config:set LINE_CHANNEL_ACCESS_TOKEN=C6HvXpZ/biiV1QHrzLeQTWFPVhZo4kICW8s7bSFRbFG5Oo4irGEGx8Gmx+TResYLTQLYXcBKHeSW+FCVZ7NuF4FBjWPxoOFwRalf3sCCx64a0PxVQVi/DtrVVmjXorYy1EFtTkBGgR8MhuxuHZ0ssgdB04t89/1O/w1cDnyilFU= --app line2nombey
-# heroku config:set LINE_CHANNEL_SECRET=60778b49be8dd61d2537eb1e616e0bfb --app line2nombey
-# heroku config:set SLACK_WEB_HOOKS_URL=https://hooks.slack.com/services/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 --app line2nombey
-
-# 認証情報の取得
-CHANNEL_ACCESS_TOKEN = os.environ.get("BIDIMTF3bcJZl86K9h2iy9H8BZQf+E0jz5so+L0cXNvbvjTxTa4FiK0bOi40Ra9M8q+CI32ROyHFxIejnosFjQlz/7IFRytCxYkWs2ftI6EKxInc5ONHcH988+YlcsWGcxXQLRK2yIjf2O5JzWEVqgdB04t89/1O/w1cDnyilFU=")
-CHANNEL_SECRET = os.environ.get("60778b49be8dd61d2537eb1e616e0bfb")
-WEB_HOOK_LINKS = os.environ.get("https://hooks.slack.com/services/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")
+CHANNEL_ACCESS_TOKEN = os.environ["C6HvXpZ/biiV1QHrzLeQTWFPVhZo4kICW8s7bSFRbFG5Oo4irGEGx8Gmx+TResYLTQLYXcBKHeSW+FCVZ7NuF4FBjWPxoOFwRalf3sCCx64a0PxVQVi/DtrVVmjXorYy1EFtTkBGgR8MhuxuHZ0ssgdB04t89/1O/w1cDnyilFU="]
+CHANNEL_SECRET = os.environ["60778b49be8dd61d2537eb1e616e0bfb"]
+WEB_HOOK_LINKS = os.environ["https://hooks.slack.com/services/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"]
+BOT_OAUTH = os.environ["xoxb-479740998288-3188861833653-0lQq610NxDVvEvI2BzoQM0BP"]
+POST_CHANNEL_ID = os.environ["C035JRRDFGD"]
+USER_OAUTH = os.environ["SLACK_USER_OAUTH"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+### 以下省略 ###
 
-
-@app.route("/callback", methods=('POST'))
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle web hook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-def get_event_info(event):
+# image file
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
     """
-    トーク情報の取得
-    :param event: LINE メッセージイベント
-    :return: ユーザID, ユーザ表示名, 送信元トークルームの種別, ルームID
-    :rtype: str, str, str, str
+    Image Message
     """
-
-    # LINEユーザー名の取得
-    user_id = event.source.user_id
-    try:
-        user_name = line_bot_api.get_profile(user_id).display_name
-    except LineBotApiError as e:
-        user_name = "Unknown"
-
-    # トーク情報の取得
-    if event.source.type == "user":
-        msg_type = "個別"
-        room_id = None
-        return user_id, user_name, msg_type, room_id
-
-    if event.source.type == "group":
-        msg_type = "グループ"
-        room_id = event.source.group_id
-        return user_id, user_name, msg_type, room_id
-
-    if event.source.type == "room":
-        msg_type = "複数トーク"
-        room_id = event.source.room_id
-        return user_id, user_name, msg_type, room_id
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    """
-    Text Message の処理
-    """
-    slack_info = slackweb.Slack(url=WEB_HOOK_LINKS)
-
-    # トーク情報の取得
+    
+    #get talk
     user_id, user_name, msg_type, room_id = get_event_info(event)
 
-    # slack側に投稿するメッセージの加工
-    send_msg = "[bot-line] {user_name}さん\n".format(user_name=user_name) \
-               + "{msg}\n".format(msg=event.message.text) \
+    #send lineImage recieve
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+    img = message_content.content #画像データ
+    # img = "/Users/okawarayu/Desktop/py_line_slack/sp_test_height.JPG"
+
+    #slack
+    send_msg = "[bot-line] {user_name} 画像を送信．\n".format(user_name=user_name) \
                + "---\n" \
                + "送信元: {msg_type} ( {room_id} )\n".format(msg_type=msg_type, room_id=room_id) \
                + "送信者: {user_name} ( {user_id} )".format(user_name=user_name, user_id=user_id)
 
-    # メッセージの送信
-    slack_info.notify(text=send_msg)
+    file_name = "send_image_{message_id}".format(message_id=message_id)
+    
+    # getimg = requests.get(img)
+    # pillowimg = Image.open(BytesIO(getimg.content))
+    # flipped_img = ImageOps.flip(pillowimg)
+    # flipped_img.show()
 
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    #send image
+    url = 'https://slack.com/api/files.upload'
+    headers = {"Authorization" : "Bearer "+ USER_OAUTH}
+    files = {'file': img}
+    param = {
+        'user': user_id,
+        # 'token': BOT_OAUTH,
+        'channels': POST_CHANNEL_ID,
+        'filename': file_name,
+        'initial_comment': send_msg,
+        'title': file_name,
+    }
+    print("!!! send slack log !!!", param)
+    res = requests.post(url, params=param, files=files, headers=headers)
+    print("res", res.json())
